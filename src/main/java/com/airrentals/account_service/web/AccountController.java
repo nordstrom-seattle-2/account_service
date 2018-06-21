f66c9f0b5ed03b92ea0d210c5811704623664242
Joshua Oliphant <joshua.oliphant@nordstrom.com>
2018-06-21 11:55:45 -0700

    Added swagger docs. Changed name of main class to Appliation.

--- src/main/java/com/airrentals/account_service/web/AccountController.java
+++ src/main/java/com/airrentals/account_service/web/AccountController.java
@@ -3,6 +3,8 @@ package com.airrentals.account_service.web;
 import com.airrentals.account_service.domain.DomainService;
 import com.airrentals.account_service.model.Member;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,8 @@ import java.io.IOException;
 
 @RestController
 @Log4j2
+@RequestMapping("/accounts")
+@Api(value = "accounts")
 public class AccountController {
 
     private final DomainService domainService;
@@ -21,6 +25,7 @@ public class AccountController {
     }
 
     @PostMapping("/addMember")
+    @ApiOperation(value = "Add a new member", response = Member.class)
     public Member addMember(@RequestBody String member) throws IOException {
         log.info("Request body: {}", member);
         Member mapped = objectMapper.readValue(member, Member.class);
